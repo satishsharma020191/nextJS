a71e88d25733557a7f970ab65e78fcf626897bcb
Satish Sharma <satsharm1@publicisgroupe.net>
2019-11-26 15:57:32 +0530

    getInitialProps server client

--- next-app/pages/index.js
+++ next-app/pages/index.js
@@ -1,20 +1,64 @@
 import Layout from '../components/Layout';
 import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
 
-const PostLink = (props) => {
-  return (<li><Link href="/p/[id]" as={`/p/${props.id}`} >
-    <a>{props.id}</a>
-  </Link>
-  </li>);
-}
+const Index = props => {
+  // console.log('print props::', props);
+  const PostLink = ({ sw }) => (
+    <li key={sw.id}>
+      <Link href="/p/[id]" as={`/p/${sw.id}`}>
+        <a>{sw.name}</a>
+      </Link>
 
-export default function Blog() {
+      <style jsx>{`
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+        font-family: 'Arial';
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
+    </li>
+  );
+
   return (<Layout>
-    <h1>My Blog</h1>
+    <h1>Tv shows</h1>
     <ul>
-      <PostLink id="Hello-Next.js" />
-      <PostLink id="learn-Next.js" />
-      <PostLink id="deploy-Next.js" />
+      {props.shows.map((sw) => {
+        return <PostLink sw={sw} />
+      })}
+
     </ul>
-  </Layout>);
+
+    <style jsx>{`
+        h1,
+        a {
+          font-family: 'Arial';
+        }
+
+        ul {
+          padding: 0;
+        }
+      `}</style>
+
+
+  </Layout>)
 }
+
+Index.getInitialProps = async function () {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+  const data = await res.json();
+  console.log(`Show data fetched. Count: ${data.length}`);
+  return {
+    shows: data.map(entry => entry.show)
+  };
+}
+export default Index;
--- next-app/pages/p/[id].js
+++ next-app/pages/p/[id].js
@@ -1,11 +1,23 @@
-import { useRouter } from 'next/router';
 import Layout from '../../components/Layout';
+//import Markdown from 'react-markdown';
+import fetch from 'isomorphic-unfetch';
 
-export default function Post() {
-    const router = useRouter();
-    console.log('printing routes::', router.query);
+const Post = props => {
+    // console.log('post props', props);
     return (<Layout>
-        <h1>{router.query.id}</h1>
-        <p>this is the blog post content</p>
+        <h1>{props.show.name}</h1>
+        <p>{props.show.summary.replace(/<[/]?[pb]>/g, '')}</p>
+        <img src={props.show.image.medium} />
     </Layout>);
 }
+
+Post.getInitialProps = async function (context) {
+    const { id } = context.query;
+    const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+    const show = await res.json();
+
+    console.log('printing context id', show.name);
+    return { show };
+}
+
+export default Post;
